8669e8aa232ef26d2dabe7c3091b43495457dd01
thorarinngunnar <thorarinnnemi@gmail.com>
2020-09-03 15:12:56 +0000

    working code

--- max_int.py
+++ max_int.py
@@ -1,2 +1,9 @@
-
-jk
+num_int = int(input("Input a number: "))    # Do not change this line
+# Fill in the missing code
+max_int = num_int
+while num_int >= 0:
+    num_int = int(input("Input a number: "))
+    if num_int > max_int:
+        max_int = num_int
+else:
+    print("The maximum is", max_int)    # Do not change this line
